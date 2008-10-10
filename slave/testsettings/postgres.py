c02f8323a686a5ef5bfdf59c729d705ec7f97faf
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-10-10 14:03:30 -0500

    Modified postgres settings template to correctly work with multiple branches.

--- slave/testsettings/postgres.py
+++ slave/testsettings/postgres.py
@@ -1,8 +1,13 @@
+import os
 import tempfile
 
 CACHE_BACKEND = "file://" + tempfile.gettempdir()
 ROOT_URLCONF = 'testsettings'
 SECRET_KEY = 'mkisz0$z*2b&7m8!^#^_t&5n7ose%hqa3$m2h$9@jhy7xk#_$2'
 DATABASE_ENGINE = 'postgresql_psycopg2'
-DATABASE_NAME = 'djangotester'
+DATABASE_NAME = 'template1'
 DATABASE_USER = 'jacob'
+
+# Make sure we get a distinct test database name -- this
+# settings file might be used to run multiple tests.
+TEST_DATABASE_NAME = "djangotests_%s" % (os.getpid())
