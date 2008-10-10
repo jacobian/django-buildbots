12df17f97e4272f5d9ce1ca9eb2a628e7278b0c6
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-10-10 12:08:59 -0500

    Add multiple slaves with different test settings as an example of whart that'd look like.'=

---
+++ slave/testsettings/postgres.py
@@ -0,0 +1,8 @@
+import tempfile
+
+CACHE_BACKEND = "file://" + tempfile.gettempdir()
+ROOT_URLCONF = 'testsettings'
+SECRET_KEY = 'mkisz0$z*2b&7m8!^#^_t&5n7ose%hqa3$m2h$9@jhy7xk#_$2'
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'djangotester'
+DATABASE_USER = 'jacob'
